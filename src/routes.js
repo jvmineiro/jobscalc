3aa7134090bf7b841fdec814cdf9988cd903f470
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-03-30 20:09:50 -0300

    Servidor atualizado

--- src/routes.js
+++ src/routes.js
@@ -6,15 +6,26 @@ const views = __dirname + "/views/"
 
 const profile = {
     name: "Joao",
-    avatar: "https://avatars.githubusercontent.com/u/31699929?v=4",
+    avatar: "https://github.com/jvmineiro.png",
     "monthly-budget": 3000,
     "hours-per-day": 5,
     "days-per-week": 5,
     "vacation-per-year": 4 
 }
 
+const jobs = []
+
 routes.get('/', (req, res) => res.render(views + "index"))
 routes.get('/job', (req, res) => res.render( views + "job"))
+routes.post('/job', (req, res) => {
+    // req.body  = { name: 'asd', 'daily-hours': '3.1', 'total-hours': '3' }
+    
+    const job = req.body
+    job.createdAt = Date.now() // Atribuindo nova DATA 
+    
+    jobs.push(req.body)
+    return res.redirect('/')
+})
 routes.get('/job/edit', (req, res) => res.render( views + "job-edit"))
 routes.get('/profile', (req, res) => res.render( views + "profile", {profile: profile}))
 
