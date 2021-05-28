b2efb72e20f34d93f3d9451956978f357a93ed6a
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-05-28 15:01:39 -0300

    Configurando o Banco

--- src/db/init.js
+++ src/db/init.js
@@ -6,13 +6,41 @@ Database.exec(`CREATE TABLE profile (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     avatar TEXT,
-    monthly-budget INT,
-    days-per-week INT, 
-    hours-per-day INT,
-    vacation-per-year INT,
-    value-hour INT
+    monthly_budget INT,
+    days_per_week INT, 
+    hours_per_day INT,
+    vacation_per_year INT,
+    value_hour INT
 )`);
 
-Database.exec()
+Database.exec(`CREATE TABLE jobs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT, 
+    daily_hours INT,
+    total_hours INT,
+    created_at DATETIME 
+)`)
+
+Database.run(`INSERT INTO profile (
+    name, 
+    avatar,
+    monthly_budget,
+    days_per_week,
+    hours_per_day,
+    vacation_per_year,
+) VALUES (
+    "Joao",
+    "htpps://github.com/jvmineiro.png",
+    3000,
+    5,
+    5,
+    4,
+);`)
  
 Database.close()
+
+// INSERT INTO jobs (
+//     name.
+//     daily_hours,
+
+// )
