5ce9c7944ffc52adc05b67a346e35f2eac9653fc
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-05-31 11:12:10 -0300

    async/await configured

--- src/db/config.js
+++ src/db/config.js
@@ -1,10 +1,9 @@
-const sqlite3 = require('sqlite3');
-const { open } = require('sqlite');
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
 
 //ligação com o banco de dados
-module.exports = () => {
-    open({
-        filename: './database.sqlite',
-        driver: sqlite3.Database,
-    }); 
-};
+module.exports = () =>
+  open({
+    filename: "./database.sqlite",
+    driver: sqlite3.Database,
+  });
--- src/db/init.js
+++ src/db/init.js
@@ -1,64 +1,70 @@
-const Database = require('config')
+const Database = require("./config");
 
-Database()
+const initDb = {
+  async init() {
+    const db = await Database()
 
-Database.exec(`CREATE TABLE profile (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    avatar TEXT,
-    monthly_budget INT,
-    days_per_week INT, 
-    hours_per_day INT,
-    vacation_per_year INT,
-    value_hour INT
-)`);
+    await db.exec(`CREATE TABLE profile (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        avatar TEXT,
+        monthly_budget INT,
+        days_per_week INT, 
+        hours_per_day INT,
+        vacation_per_year INT,
+        value_hour INT
+    )`)
 
-Database.exec(`CREATE TABLE jobs (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT, 
-    daily_hours INT,
-    total_hours INT,
-    created_at DATETIME 
-)`)
+    await db.exec(`CREATE TABLE jobs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT, 
+        daily_hours INT,
+        total_hours INT,
+        created_at DATETIME 
+    )`)
 
-Database.run(`INSERT INTO profile (
-    name, 
-    avatar,
-    monthly_budget,
-    days_per_week,
-    hours_per_day,
-    vacation_per_year,
-) VALUES (
-    "Joao",
-    "htpps://github.com/jvmineiro.png",
-    3000,
-    5,
-    5,
-    4,
-);`)
+    await db.run(`INSERT INTO profile (
+        name, 
+        avatar,
+        monthly_budget,
+        days_per_week,
+        hours_per_day,
+        vacation_per_year
+    ) VALUES (
+        "Joao",
+        "htpps://github.com/jvmineiro.png",
+        3000,
+        5,
+        5,
+        4
+    )`)
 
-Database.run(`INSERT INTO jobs (
-    name.
-    daily_hours,
-    total_hours,
-    created-at,
-) VALUES (
-    "Pizzaria Guloso",
-    2,
-    1,
-    16175143766018
-);`)
+    await db.run(`INSERT INTO jobs (
+        name,
+        daily_hours,
+        total_hours,
+        created_at
+    ) VALUES (
+        "Pizzaria Guloso",
+        2,
+        1,
+        16175143766018
+    )`)
 
-Database.run(`INSERT INTO jobs (
-    name.
-    daily_hours,
-    total_hours,
-    created-at,
-) VALUES (
-    "OneTwo Projects",
-    3,
-    47,
-    16175143766018
-);`)
- 
-Database.close()
+    await db.run(`INSERT INTO jobs (
+        name,
+        daily_hours,
+        total_hours,
+        created_at
+    ) VALUES (
+        "OneTwo Projects",
+        3,
+        47,
+        16175143766018
+    )`)
+
+    await db.close();
+  }
+}
+
+initDb.init()
