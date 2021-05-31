bcb47f1bc61a7fa34a0a1a2100c193ade295bb92
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-05-31 13:42:44 -0300

    Db Profile done ✔👌

--- src/model/Profile.js
+++ src/model/Profile.js
@@ -2,6 +2,7 @@ const Database = require('../db/config')
 
 module.exports = {
     async get(){
+      //abrir a conexao com o DB
       const db = await Database()
 
       const data = await db.get(`SELECT * FROM profile`)
@@ -19,7 +20,19 @@ module.exports = {
       };
     },
 
-    update(newData){
-      data = newData;
+    async update(newData){
+      const db = await Database()
+
+      db.run(`UPDATE profile SET 
+      name = "${newData.name}",
+      avatar = "${newData.avatar}",
+      monthly_budget = ${newData["monthly-budget"]},
+      days_per_week = ${newData["days-per-week"]},
+      hours_per_day = ${newData["hours-per-day"]},
+      vacation_per_year = ${newData["vacation-per-year"]},
+      value_hour = ${newData["value-hour"]}
+      `)
+
+      await db.close()
     }
 }
